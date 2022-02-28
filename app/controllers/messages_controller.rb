387622b83214a2d3e24998f543db5fd8eeae8315
Keisuke Kitazumi <kitazumi.gc8@gmail.com>
2022-02-28 14:04:05 +0900

    チャットルーム名の表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
